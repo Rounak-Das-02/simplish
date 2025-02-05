98f23dc4043607cca935a9fca16588917ea18eb1
Rounak <rounakd@hike.in>
2025-02-05 13:13:36 +0530

    Building sites

---
+++ static/js/dark-mode.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", () => {
+// 	const toggle = document.getElementById("darkModeToggle");
+// 	const darkMode = localStorage.getItem("darkMode") === "enabled";
+
+// 	if (darkMode) document.body.classList.add("dark");
+
+// 	toggle.addEventListener("click", () => {
+// 		document.body.classList.toggle("dark");
+// 		localStorage.setItem(
+// 			"darkMode",
+// 			document.body.classList.contains("dark") ? "enabled" : "disabled"
+// 		);
+// 	});
+// });
